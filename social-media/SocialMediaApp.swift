41754a129998ccf0d545601281eeeb6159617b76
Eduardo Dias <eduardo.dias@iag.co.nz>
2021-07-12 19:32:39 +1200

    Add initial project files

---
+++ social-media/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  social-media
+//
+//  Created by Eduardo Dias on 12/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
